e6d33bfed52d6b32625b83057ab4968c979b5951
rrossette <rossette_renan@hotmail.com>
2023-09-11 09:44:23 -0400

    :bug:

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use(cors())
-app.use(express.json())
-app.use(express.limit('1gb'))
+app.use(express.json({
+    limit: '1024MB'
+}))
 
 app.get('/', (req, res) => {
     res.status(200).send({ msg: "Funcionando!!!" })
